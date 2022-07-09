4056855e4f50eae06d31ea3bf96d1ae490268757
Nguyễn Văn Thy <coder.haibach@gmail.com>
2022-07-09 17:59:21 +0700

    Init

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+import Navigation from './Navigation.vue'
+</script>
+
+
+<template>
+<div class="wrapper header logo-left">
+  <div class="container">
+    <!-- Logo -->
+    <a class="logo" href="../templates/" data-mScale="100">RubySlider</a>
+
+    <!-- Menu -->
+    <!-- <nav data-rubymenu></nav> -->
+    <Navigation />
+  </div>
+</div>
+</template>
