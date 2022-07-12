822bba953aee725c173b4a2901c07c3e5a577465
van_thy <coder.haibach@gmail.com>
2022-07-12 17:01:04 +0700

    Install jquery to package

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.scss'
+// import $ from 'jquery'
 // import './assets/js/jquery-last.min'
 // import './assets/js/ruby01'
 // import './assets/js/rubyanimate'
 // import './assets/js/rubyslider'
+
+// window.jQuery = $
 
 const app = createApp(App)
 
